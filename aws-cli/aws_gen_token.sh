08878ff87d1bd19e5cdc9438103e404f1e16c8f8
s33ding <robertomdiniz@protonmail.com>
2022-12-29 19:18:01 -0300

    saving

--- aws-cli/aws_gen_token.sh
+++ aws-cli/aws_gen_token.sh
@@ -1,6 +1,5 @@
-cp $aws_cred_place ~/.aws/credentials
-token_py=~/ADM/aws/token.py
+cp $aws_cred_place $aws_cred
 read -p 'TOKEN: ' -s token
-aws sts get-session-token --duration-seconds 3600 --serial-number  $arn_aws --token-code $token >> ~/.aws/.tmp.json
+aws sts get-session-token --duration-seconds 3600 --serial-number  $arn_aws --token-code $token >> $aws_temp_cred
 python3 $token_py
 rm -r ~/.aws/.tmp.json

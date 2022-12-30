33aea6f7783ae4c2f831d2c7e936adf1a321f0c5
s33ding <robertomdiniz@protonmail.com>
2022-12-29 22:05:31 -0300

    saving

--- aws-cli/generate_token/aws_gen_token.sh
+++ aws-cli/generate_token/aws_gen_token.sh
@@ -1,6 +1,6 @@
-cp $aws_cred_place $aws_cred
-read -p 'TOKEN: ' -s token
-aws sts get-session-token --duration-seconds 3600 --serial-number  $arn_aws --token-code $token >> $aws_temp_cred
-python3 $token_py
-rm -r $aws_temp_cred
+cp $AWS_CRED_PLACE $AWS_CRED
+read -p 'TOKEN: ' -s TOKEN
+aws sts get-session-token --duration-seconds 3600 --serial-number  $ARN_AWS --token-code $TOKEN >> $AWS_TEMP_CRED
+python3 $TOKEN_PY
+rm -r $AWS_TEMP_CRED
 
--- aws-cli/generate_token/token.py
+++ aws-cli/generate_token/token.py
@@ -1,13 +1,13 @@
 import os 
 import json 
 
-with open(os.environ['aws_temp_cred'],'r') as f:
+with open(os.environ['AWS_TEMP_CRED'],'r') as f:
     dt=json.load(f)
 
 aws = dt.get('Credentials')
 
 os.system("touch ~/.aws/credentials")
-with open(os.environ['aws_cred'],'w') as f:
+with open(os.environ['AWS_CRED'],'w') as f:
     f.write(f"[default]\n")
     f.write(f"aws_access_key_id = {aws['AccessKeyId']}\n")
     f.write(f"aws_secret_access_key = {aws['SecretAccessKey']}\n")

a47c90c144bd62837cd4a22c5c9a78f087657808
s33ding <robertomdiniz@protonmail.com>
2023-01-06 17:08:01 -0300

    saving

---
+++ Boto3/paramter_store.py
@@ -0,0 +1,19 @@
+import sys
+import boto3
+import json
+import os
+
+with open(os.environ["AWS_KEY"]) as file:
+    cred = json.load(file)
+
+
+client = boto3.client('ssm', 
+                      region_name='us-east-1', 
+                      aws_access_key_id=cred.get('usr'), 
+                      aws_secret_access_key=cred.get('passwd')
+                      )
+
+paramter_name = sys.argv[1]
+request = client.get_parameter(Name = paramter_name, WithDecryption=True)
+result = request['Parameter']['Value']
+print(result)
--- aws-cli/RDS-OFF.sh
+++
@@ -1,1 +0,0 @@
-aws rds stop-db-instance --db-instance-identifier $RDS_INSTANCE --region $AWS_REGION --profile $AWS_PROFILE

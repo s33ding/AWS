d5e25187b2b88a40169e0a1f1c39b1ebd1115ff7
s33ding <robertomdiniz@protonmail.com>
2023-02-27 21:40:04 -0300

    saving

--- generate_token_mfa/aws_gen_token.sh
+++ generate_token_mfa/aws_gen_token.sh
@@ -1,4 +1,4 @@
 python3 $TOKEN_A
 read -p 'TOKEN: ' -s TOKEN
-aws sts get-session-token --duration-seconds 3600 --serial-number  $ARN_AWS --token-code $TOKEN > $AWS_TEMP_CRED
+aws sts get-session-token --duration-seconds 4600 --serial-number  $ARN_AWS --token-code $TOKEN > $AWS_TEMP_CRED
 python3 $TOKEN_B

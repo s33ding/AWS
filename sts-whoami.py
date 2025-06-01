460dcf8912ec19307e639c4a09f7f2ff3c75cd3b
s33ding <robertomdiniz@protonmail.com>
2025-06-01 13:08:14 -0300

    showing the aws-region in the sts func

--- sts-whoami.py
+++ sts-whoami.py
@@ -1,7 +1,13 @@
 import boto3
 
-# Create an STS client
-sts_client = boto3.client('sts')
+# Create a Boto3 session
+session = boto3.Session()
+
+# Get the default region from the session
+region = session.region_name
+
+# Create an STS client using the session
+sts_client = session.client('sts')
 
 # Call get_caller_identity
 identity = sts_client.get_caller_identity()
@@ -10,4 +16,5 @@ identity = sts_client.get_caller_identity()
 print("Account ID:", identity['Account'])
 print("User ID:", identity['UserId'])
 print("ARN:", identity['Arn'])
+print("Region:", region)
 

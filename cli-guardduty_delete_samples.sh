85421fac90ea7384434b30b26cb7b0f53627a1fc
s33ding <robertomdiniz@protonmail.com>
2025-11-16 20:08:07 -0300

    Update bash script to use Python version for GuardDuty sample cleanup

--- cli-guardduty_delete_samples.sh
+++ cli-guardduty_delete_samples.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-# Get detector ID
-DETECTOR_ID=$(aws guardduty list-detectors --query 'DetectorIds[0]' --output text)
-
-# List sample findings
-aws guardduty get-findings --detector-id $DETECTOR_ID --finding-ids $(aws guardduty list-findings --detector-id $DETECTOR_ID --finding-criteria '{"Sample":{"Eq":["true"]}}' --query 'FindingIds' --output text)
-
-# Archive sample findings (this effectively removes them from active view)
-aws guardduty archive-findings --detector-id $DETECTOR_ID --finding-ids $(aws guardduty list-findings --detector-id $DETECTOR_ID --finding-criteria '{"Sample":{"Eq":["true"]}}' --query 'FindingIds' --output text)
+# Archive all GuardDuty sample findings using Python script
+python3 guardduty_delete_samples.py

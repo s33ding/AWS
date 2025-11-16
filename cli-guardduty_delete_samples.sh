5aa54a88cf916038af9b79499e3c3c6475a22378
s33ding <robertomdiniz@protonmail.com>
2025-11-16 19:50:37 -0300

    Fix case sensitivity: Sample field should be uppercase

--- cli-guardduty_delete_samples.sh
+++ cli-guardduty_delete_samples.sh
@@ -4,7 +4,7 @@
 DETECTOR_ID=$(aws guardduty list-detectors --query 'DetectorIds[0]' --output text)
 
 # List sample findings
-aws guardduty get-findings --detector-id $DETECTOR_ID --finding-ids $(aws guardduty list-findings --detector-id $DETECTOR_ID --finding-criteria '{"sample":{"Eq":["true"]}}' --query 'FindingIds' --output text)
+aws guardduty get-findings --detector-id $DETECTOR_ID --finding-ids $(aws guardduty list-findings --detector-id $DETECTOR_ID --finding-criteria '{"Sample":{"Eq":["true"]}}' --query 'FindingIds' --output text)
 
 # Archive sample findings (this effectively removes them from active view)
-aws guardduty archive-findings --detector-id $DETECTOR_ID --finding-ids $(aws guardduty list-findings --detector-id $DETECTOR_ID --finding-criteria '{"sample":{"Eq":["true"]}}' --query 'FindingIds' --output text)
+aws guardduty archive-findings --detector-id $DETECTOR_ID --finding-ids $(aws guardduty list-findings --detector-id $DETECTOR_ID --finding-criteria '{"Sample":{"Eq":["true"]}}' --query 'FindingIds' --output text)
